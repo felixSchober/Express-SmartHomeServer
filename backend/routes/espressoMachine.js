4f465402020c335827ca618d0aa1dff43b1082df
Felix Schober <felix.schober@tum.de>
2017-07-24 18:41:46 +0200

    add number of espressos drunk in total and per week

--- backend/routes/espressoMachine.js
+++ backend/routes/espressoMachine.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var EspressoMachine = require('./../models/espresso')
 
 
-/* GET espresso status. */
+/* GET espresso status.
+ * /api/espresso/
+ */
 router.get('/', function(req, res, next) {
 	
 	// get the espresso object from mongoose
@@ -13,7 +15,9 @@ router.get('/', function(req, res, next) {
 		res.json(espressoMachine);
 	});
 });
-
+/* POST New Espresso
+ * /api/espresso/
+ */
 router.post('/', function (req, res, next) {
 	
 	// get the espresso object from mongoose
@@ -33,6 +37,10 @@ router.post('/', function (req, res, next) {
 	});
 })
 
+
+/* POST New Espresso Machine
+ * /api/espresso/machine/
+ */
 router.post('/machine/', function (req, res, next) {
 	EspressoMachine.create({
 		name : 'Krupps Espresso',
@@ -43,5 +51,65 @@ router.post('/machine/', function (req, res, next) {
 		res.json(espressoMachine);
 	})
 })
+
+/* GET total number of espressos drunk
+ * /api/espresso/statistic/total
+ */
+router.get('/statistic/total/', function(req, res, next) {
+	
+	// get the espresso object from mongoose
+	EspressoMachine.getEspressoMachine(function (err, espressoMachine) {
+		if (err) res.send(err);
+		
+		const numberOfEspressos = espressoMachine.espressos.length;
+		const response = {
+			name: espressoMachine.name,
+			value: numberOfEspressos
+		}
+		res.json(response);
+	});
+});
+
+/* GET number of espressos drunk this week
+ * /api/espresso/statistic/total
+ */
+router.get('/statistic/week/', function(req, res, next) {
+	console.log('Got /statistic/week/ request.');
+	
+	// get the espresso object from mongoose
+	EspressoMachine.getEspressoMachine(function (err, espressoMachine) {
+		if (err) res.send(err);
+		
+		const firstDayOfCurrentWeek = getFirstAndLastDayOfWeek().monday;
+		let numberOfEspressos = 0;
+		for (let i = espressoMachine.espressos.length - 1; i >= 0; i--) {
+			const espresso = espressoMachine.espressos[i];
+			if (espresso.created >= firstDayOfCurrentWeek) {
+				numberOfEspressos++;
+			} else {
+				// we've gone to far in the past so let's stop here
+				break;
+			}
+		}
+		const response = {
+			name: espressoMachine.name,
+			since: firstDayOfCurrentWeek,
+			value: numberOfEspressos
+		}
+		res.json(response);
+	});
+});
+
+function getFirstAndLastDayOfWeek() {
+	var today, todayNumber, mondayNumber, sundayNumber, monday, sunday;
+	today = new Date();
+	todayNumber = today.getDay();
+	mondayNumber = 1 - todayNumber;
+	sundayNumber = 7 - todayNumber;
+	monday = new Date(today.getFullYear(), today.getMonth(), today.getDate()+mondayNumber);
+	sunday = new Date(today.getFullYear(), today.getMonth(), today.getDate()+sundayNumber );
+	
+	return {monday: monday, sunday: sunday};
+}
 
 module.exports = router;
