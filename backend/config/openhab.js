cc56aebf46d99bb5f4f3cdb6e16dfde5de92b015
Felix Schober <felix.schober@tum.de>
2017-12-18 19:00:38 +0100

    add route to change TV channel

--- backend/config/openhab.js
+++ backend/config/openhab.js
@@ -1,4 +1,10 @@
 module.exports = {
 	url: '192.168.178.38',
-	port: '8080'
+	port: '8080',
+	harmonyDeviceButtonPressItems: {
+		'onkyo': 'OnkyoAVReceiver_SendButtonPress',
+		'tv': 'TV_SendButtonPress ',
+		'xbox': 'XboxOne_SendButtonPress',
+		'appletv': 'AtlantisAppleTVHarmony_SendButtonPress '
+	}
 }
