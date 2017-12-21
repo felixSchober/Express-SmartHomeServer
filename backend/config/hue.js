7bde2c76087f473c6ada1fda00dcb50155ed9089
Felix Schober <felix.schober@tum.de>
2017-12-21 13:15:16 +0100

    Completely rewrite hue route

--- backend/config/hue.js
+++ backend/config/hue.js
@@ -1,10 +1,14 @@
-const hueIp = '192.168.178.20'
-const hueUser = 'UJL3qt0jG-zI0eqhj8w6Ae5b4c9Msq-WwGztDmAu'
+const hueIp = '192.168.178.20';
+const hueUser = 'UJL3qt0jG-zI0eqhj8w6Ae5b4c9Msq-WwGztDmAu';
+
+const hueMotionSensorNameIdMapping = {
+	'kitchen': 57,
+	'entrance': 64
+};
 
 module.exports = {
 	hueIp: '192.168.178.20',
 	hueUser: 'UJL3qt0jG-zI0eqhj8w6Ae5b4c9Msq-WwGztDmAu',
 	hueUrl : hueIp + '/api/' + hueUser + '/',
-	motionSensorTempKitchenId: 57,
-	motionSensorTempEntranceId: 64
+	hueMotionSensorNameIdMapping: hueMotionSensorNameIdMapping
 };
