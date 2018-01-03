f50dbdae52504bba84df31035c56049ac96c10f6
Felix Schober <felix.schober@tum.de>
2018-01-03 15:40:57 +0100

    new energy model & remove db upload (for now)

--- backend/models/energy.js
+++ backend/models/energy.js
@@ -8,11 +8,40 @@ var energySchema = new mongoose.Schema({
 	created: {type: Date, default: Date.now()}
 });
 
+var energyDayLog = new mongoose.Schema({
+	log: [
+		[Number], // 00:00
+		[Number], // 01:00
+		[Number], // 02:00
+		[Number], // 03:00
+		[Number], // 04:00
+		[Number], // 05:00
+		[Number], // 06:00
+		[Number], // 07:00
+		[Number], // 08:00
+		[Number], // 09:00
+		[Number], // 10:00
+		[Number], // 11:00
+		[Number], // 12:00
+		[Number], // 13:00
+		[Number], // 14:00
+		[Number], // 15:00
+		[Number], // 16:00
+		[Number], // 17:00
+		[Number], // 18:00
+		[Number], // 19:00
+		[Number], // 20:00
+		[Number], // 21:00
+		[Number], // 22:00
+		[Number], // 23:00
+	]
+});
+
 var plugEnergyStateSchema = new mongoose.Schema({
 	name: String,
 	host: String,
 	updated: { type: Date, default: Date.now() },
-	energyLog: [energySchema]
+	energyLog: [energyDayLog]
 });
 
 plugEnergyStateSchema.statics.getPlugEnergyHistory = function (plugName, callback) {
--- backend/recurringUpdates.js
+++ backend/recurringUpdates.js
@@ -6,10 +6,19 @@ const moment = require('moment');
 module.exports.pollPowerState = schedule
 	.scheduleJob('*/' + hs110Plugs.energyHistoryUpdateEveryXSeconds + ' * * * * *', function () {
 		console.log('[Schedule] - ' + moment().format() + ':\tStarting power update');
-		hs110Plugs.updatePowerStateAndSaveToDb()
-})
+		hs110Plugs.updatePowerState()
+});
+
+// save history every hour
+module.exports.savePowerStateToDb = schedule
+.scheduleJob('59 * * * *', function () {
+	console.log('[Schedule] - ' + moment().format() + ':\tSave PowerState to DB');
+	// TODO: Solve problem!
+	//hs110Plugs.savePowerStateToDb()
+});
+
 module.exports.pollNewEspresso = schedule.scheduleJob('*/1 * * * *', function () {
 	console.log('[Schedule] - ' + moment().format() + ':\tStarting espresso check');
 	espresso.checkIfNewEspressoHasBeenCreated();
-})
+});
 
