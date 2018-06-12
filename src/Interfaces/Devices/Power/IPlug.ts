a49adc541d3237587d82314d8707375d4136aac6
Felix Schober <github@felix-schober.com>
2018-06-12 22:31:31 +0200

    combine typescript scripts with node js server

---
+++ src/Interfaces/Devices/Power/IPlug.ts
@@ -0,0 +1,17 @@
+import {ITuple} from '../../ITuple';
+
+export interface IPlug {
+	name: string;
+	currentPowerState: number;
+	ip: string;
+	isOn: boolean;
+
+	getLivePowerForPlug(): Promise<ITuple<string, number>>;
+	getCachedPowerForPlug(): number;
+	getRawPlugState(): Promise<any>;
+	isPlugRelayOn(): Promise<boolean>;
+
+	updatePlugState(stateOn: boolean): Promise<boolean>;
+	togglePlugState(): Promise<boolean>;
+	registerPlugPowerEvent(callbackOn: ()=>void, callbackOff: ()=>void): void;
+}
---
+++ src/Services/Socket/LightSocketService.ts
@@ -0,0 +1,80 @@
+import {IAggregatedLightResult} from '../../Interfaces/Devices/Light/IAggregatedLightResult';
+import {ILightControllerService} from '../../Interfaces/Devices/Light/ILightControllerService';
+import {IDeviceController} from '../../Interfaces/IDeviceController';
+import {ISocketController} from '../../Interfaces/ISocketController';
+import {ISwitchStateChangeCommand} from '../../Interfaces/ISwitchStateChangeCommand';
+import {BaseSocketService} from './BaseSocketService';
+import {Server, Socket} from 'socket.io';
+
+export class LightSocketService extends BaseSocketService {
+
+	constructor(socketName: string,
+	            io: Server,
+	            pollingInterval: number,
+	            socketMessageIdentifier: string,
+	            controller: IDeviceController,
+	            socketController: ISocketController) {
+		super(socketName, io, socketMessageIdentifier, controller, socketController, {day: '*', hour: '*', minute: '*', second: '*/' + pollingInterval});
+	}
+
+	public sendInitialState() {
+		this.sendInitialLightStates();
+	}
+
+	public addSocketObserver(socket: Socket) {
+		this.sockets.push(socket);
+
+		// LIGHT EVENTS
+		socket.on(this.socketMessageIdentifier, (command: ISwitchStateChangeCommand) => {
+			if (!command) {
+				const logMessage = '[Lights] Received light change command via socket but message is invalid';
+				this.socketController.log(logMessage, false);
+				return;
+			}
+
+			const lightController = this.controller as ILightControllerService;
+			let promise: Promise<boolean>;
+
+			// TODO: Use enum
+			if (command.state === 'toggle') {
+				promise = lightController.toggleLightState(command.name);
+			} else {
+				const state = command.state === 'on';
+				promise = lightController.setLightState(command.name, state);
+			}
+
+			// execute promise
+			promise.then((newState: boolean) => this.socketController.log(
+				'[Lights] State change successful. New State for Light ' + command.name + ': ' + newState, false))
+			.catch((err) => this.socketController.log(
+					'[Lights] State change NOT successful. Light Name ' + command.name + ' - Error: ' + err, true));
+		});
+
+	}
+
+	public sendUpdates() {
+		const lightController = this.controller as ILightControllerService;
+		lightController.getCachedLightStateIfPossible()
+			.then((result) => this.sendLightState(result))
+			.catch((err) => this.socketController.log('Could not get light states ' + err, true));
+	}
+
+	sendInitialLightStates() {
+		const lightController = this.controller as ILightControllerService;
+
+		lightController.getLights()
+			.then((result) => this.sendLightState(result))
+			.catch((err) => this.socketController.log('Could not get initial light states ' + err, true));
+	}
+
+	private sendLightState(lightStates: IAggregatedLightResult) {
+		this.socketController.send(this.socketMessageIdentifier + '_CountTotal', lightStates.totalCount);
+		this.socketController.send( this.socketMessageIdentifier + '_CountOn', lightStates.onCount);
+		this.socketController.send( this.socketMessageIdentifier + '_CountOff', lightStates.offCount);
+
+		// send individual light states
+		for(const l of lightStates.lights){
+			this.socketController.send(this.socketMessageIdentifier + '_' + l.name, l.stateOn);
+		}
+	}
+}
